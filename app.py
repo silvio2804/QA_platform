b40ba1ad202f8aacef5de3cbcfaf52af52fd051d
silvio2804 <silvio.venturino00@gmail.com>
2024-07-13 11:19:28 +0200

    show user questions2

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for, render_template
+import pymongo
+from pymongo.errors import ConnectionFailure
+from pymongo.errors import OperationFailure
 app = Flask(__name__)
 
-("This project implements a Questions and Answer platform like Stack Overflow. "
- "The main goal is to implement a MongoDB database for our University project.")
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["question_anserw_multiple"]
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route("/home")
+def home():
+    return render_template("questions/home.html")
 
+@app.route("/myQuestions",methods=['GET'])
+def show_user_questions():
+    #Test db connection
+    if not connect_to_db():
+        return redirect("error_page.html", code=500)
 
+    '''Authentication'''
+
+    question_collection = mydb["questions"]
+    questions = question_collection.find();
+    for question in questions:
+        print(question)
+
+    return render_template("questions/home.html",questions=questions)
+
+@app.route("/myAnswers",methods=['GET'])
+def show_user_answers():
+    '''Authentication'''
+    return "showing user answers"
+
+def connect_to_db():
+    try:
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb = myclient["question_anserw_multiple_csv"]
+        print(mydb)
+    except ConnectionFailure as e:
+        print("Connection failed")
+        print(e)
+    return True
 
 if __name__ == '__main__':
     app.run()
