1a4920a51537b8a6c54ae8b6182933e5869c3cf5
silvio2804 <silvio.venturino00@gmail.com>
2024-07-15 11:37:59 +0200

    show home implementation random questions

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, redirect, url_for, render_template
 import pymongo
 from pymongo.errors import ConnectionFailure
@@ -5,15 +7,22 @@ from pymongo.errors import OperationFailure
 app = Flask(__name__)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["question_anserw_multiple"]
+mydb = myclient["qa_platform"]
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
-@app.route("/home")
-def home():
-    return render_template("questions/home.html")
+@app.route("/",methods=['GET'])
+def show_home():
+    #Test db connection
+    if not connect_to_db():
+        return redirect("error_page.html", code=500)
+
+    '''Authentication'''
+    question_collection = mydb["questions"] #riferimento alla collezione
+    questions = question_collection.find() #lista di domande
+
+    random_questions = random.sample(list(questions), 10) #lista di domande random
+
+    return render_template("questions/index.html", questions=random_questions)
 
 @app.route("/myQuestions",methods=['GET'])
 def show_user_questions():
@@ -22,13 +31,27 @@ def show_user_questions():
         return redirect("error_page.html", code=500)
 
     '''Authentication'''
-    '''github funziona?'''
     question_collection = mydb["questions"]
-    questions = question_collection.find();
-    for question in questions:
-        print(question)
+    user_collection = mydb["users"]
 
-    return render_template("questions/home.html",questions=questions)
+
+    user = user_collection.find_one({"id":14008})
+    print("Il nome utente è: ")
+    print(user)
+
+    pipeline = [
+        {
+            '$lookup': {
+                'from': 'users',
+                'localField': 'OwnerUserId',
+                'foreignField': 'id',
+                'as': 'joinedResult'
+            }
+        }
+    ]
+    user_questions = list(question_collection.aggregate(pipeline))
+    '''printare userquestions'''
+    return render_template("questions/my_questions.html", questions=user_questions)
 
 @app.route("/myAnswers",methods=['GET'])
 def show_user_answers():
@@ -38,7 +61,7 @@ def show_user_answers():
 def connect_to_db():
     try:
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        mydb = myclient["question_anserw_multiple_csv"]
+        mydb = myclient["qa_platform"]
         print(mydb)
     except ConnectionFailure as e:
         print("Connection failed")
